88bf75c3929c691764f629d10c86849ec80b6823
willidachili <groundon87@hotmail.com>
2022-11-15 13:44:41 +0100

    Added terraform config files

---
+++ apprunner.tf
@@ -0,0 +1,18 @@
+resource "aws_apprunner_service" "service" {
+  service_name = var.prefix
+  source_configuration {
+
+    authentication_configuration {
+      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
+    }
+
+    image_repository {
+      image_configuration {
+        port = "8080"
+      }
+      image_identifier      = var.image
+      image_repository_type = "ECR"
+    }
+    auto_deployments_enabled = false
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.39.0"
+    }
+  }
+  backend "s3" {
+    bucket = "pgr301-2021-terraform-state"
+    key    = "wine001/wine-hello.state"
+    region = "eu-north-1"
+  }
+}
